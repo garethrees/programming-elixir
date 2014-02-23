78caa637649a4b0e2cd805df7ee2bc2eae54f41a
Gareth Rees <gareth.h.rees@gmail.com>
2014-02-23 15:53:27 +0000

    Chapter 4

---
+++ chapter4/collection_types.exs
@@ -0,0 +1,13 @@
+# Collection Types
+
+## Tuples
+{ 1, 2 }
+{ :ok, 42, "next" }
+
+## Lists
+[1,2,3]
+
+## Keyword Lists
+[ name: "Dave", city: "Dallas", likes: "Programming" ]
+[ { :name, "Dave" }, { :city, "Dallas" }, { :likes, "Programming" } ]
+
---
+++ chapter4/value_types.exs
@@ -0,0 +1,21 @@
+# Value Types
+
+## Integers
+1000000
+1_000_000
+
+# Floats
+1.0
+0.2345
+
+# Atoms (Symbols)
+:fred
+:binary?
+:===
+:"func/3"
+
+# Regular Expressions
+%r/[aeiou]/
+
+# Ranges
+1..5
